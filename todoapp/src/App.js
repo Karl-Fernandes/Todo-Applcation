e7165966ce225e7b84e8dee9a6e908949bba94f8
Karl <krf8021@gmail.com>
2024-06-22 20:15:31 +0100

    ensured checkbox doesn't change

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -5,19 +5,18 @@ import TodoList from './TodoList';
 
 export default function App() {
   const [tasks, setTasks] = useState(() => {
-    const localValue = localStorage.getItem("ITEMS")
-    if (localValue == null) return []
-    return JSON.parse(localValue)
+    const localValue = localStorage.getItem("ITEMS");
+    if (localValue == null) return [];
+    return JSON.parse(localValue);
   });
 
   useEffect(() => {
-    localStorage.setItem("ITEMS", JSON.stringify(tasks))
-  }, [tasks])
-
+    localStorage.setItem("ITEMS", JSON.stringify(tasks));
+  }, [tasks]);
 
   // Add a new task
   const addTask = (task) => {
-    setTasks([...tasks, task]);
+    setTasks([...tasks, { ...task, completed: false }]);
   };
 
   // Handler for deleting a task
@@ -25,10 +24,20 @@ export default function App() {
     setTasks(tasks.filter(task => task.id !== id));
   };
 
+  // Handler for toggling the completed state of a task
+  const toggleTaskCompleted = (id) => {
+    setTasks(tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    }));
+  };
+
   return (
     <div className="container">
       <NewTodoForm addTask={addTask} />
-      <TodoList tasks={tasks} deleteTask={handleDelete} />
+      <TodoList tasks={tasks} deleteTask={handleDelete} toggleTaskCompleted={toggleTaskCompleted} />
     </div>
   );
 }
--- todoapp/src/TodoList.js
+++ todoapp/src/TodoList.js
@@ -1,4 +1,4 @@
-export default function TodoList({ tasks, deleteTask }) {
+export default function TodoList({ tasks, deleteTask, toggleTaskCompleted }) {
     return (
       <>
         <h1 className="header">Todo List</h1>
@@ -9,10 +9,14 @@ export default function TodoList({ tasks, deleteTask }) {
             {tasks.map((task) => (
               <li key={task.id}>
                 <label>
-                  <input type="checkbox" />
+                  <input
+                    type="checkbox"
+                    checked={task.completed}
+                    onChange={() => toggleTaskCompleted(task.id)}
+                  />
                   {task.text}
-                  <button 
-                    className="btn btn-danger" 
+                  <button
+                    className="btn btn-danger"
                     onClick={() => deleteTask(task.id)}
                   >
                     Delete
